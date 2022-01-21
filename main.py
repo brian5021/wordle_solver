79e2eccc90b9a8b227185049fff4bef31ad8895e
Brian Cox <bcox@hubspot.com>
2022-01-21 11:24:46 -0500

    Add common word preference in scoring

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import sys
 from State import State
 
 vowels = ["a", "e", "i", "o", "u"]
-size, max_guesses = 5, 6
+size, max_guesses = 5, 5
 english_words = []
+common_words = []
 state = State([], [])
+guess = ""
 
 
 def load_words(size):
@@ -19,35 +21,73 @@ def load_words(size):
         global english_words
         english_words = five_letter_words
 
+    with open('wordlist.10000.txt') as word_file:
+        five_letter_words = []
+        for word in word_file.read().split():
+            if len(word) == size:
+                five_letter_words.append(word)
+
+        global common_words
+        common_words = five_letter_words
+
 
 def first_words():
     max = 0
-    results = []
+    results = dict()
     for word in english_words:
         score = 0
+        distinct_letters = set()
         for letter in word:
+            if letter in distinct_letters:
+                continue
+
+            distinct_letters.add(letter)
             if letter in vowels:
                 score += 1
             elif letter == "y":
                 score += .5
+            else:
+                score += .25
+
+        if word in common_words:
+            score += 5
 
         if score == max:
-            results.append(word)
+            results[word] = int(score)
         elif score > max:
             max = score
-            results = [word]
+            results.clear()
+            results[word] = int(score)
 
-    return results
+    return sorted(results, key=results.get, reverse=True).pop(0), len(results.keys())
 
 
 def filter_words():
-    filtered_words = []
+    global english_words
+    filtered_words = {}
     for word in english_words:
         if contains_no_forbidden_letters(word, state.not_letters, state.cells):
             if contains_all_known_letters(word, state.letters):
-                filtered_words.append(word)
+                filtered_words[word] = int(distinct_score(word))
 
-    return filtered_words
+    english_words = filtered_words
+    return sorted(filtered_words, key=filtered_words.get, reverse=True).pop(0), len(filtered_words.keys())
+
+
+def distinct_score(word):
+    score = 0
+    distinct_letters = set()
+    for letter in word:
+        if letter not in distinct_letters:
+            if letter in vowels:
+                score += .5
+            else:
+                score += 1
+            distinct_letters.add(letter)
+
+    if word in common_words:
+        score += 100
+    return score
 
 
 def contains_all_known_letters(word, letters):
@@ -84,20 +124,25 @@ def matches_cell_input(letter, cell, not_letters):
 
 
 def init_game():
-    global size, max_guesses, state
+    global size, state, guess
     size = int(input("What is the size of the word?"))
-    state = State([],[],size)
+    state = State([], [], size)
     load_words(size)
 
-    print(random.choices(first_words(), k=3))
+    guess = first_words()[0]
+    print(guess)
 
 
 def collect_input():
-    result = input("Did we win? y/N") == "y"
-    if result:
-        return result
+    global english_words, max_guesses
+    result = int(input("What happened on our guess?\n (1) We won!\n (2) Word was wrong\n (3) Word was invalid"))
+    if result == 1:
+        return True
+    elif result == 3:
+        del english_words[guess]
+        max_guesses += 1
+        return False
 
-    guess = input("What was our guess?")
     for index, cell in state.cells.items():
         if cell.is_known():
             print("Letter " + str(index + 1) + " is already known")
@@ -113,18 +158,26 @@ def collect_input():
             else:
                 state.add_miss(guess[index])
 
-    return result
+    return False
 
 
 if __name__ == '__main__':
     init_game()
-
-    for guess in range(max_guesses):
+    while max_guesses > 0:
         we_won = collect_input()
         if we_won:
             print("Hooray!")
             sys.exit()
 
-        filtered_words = filter_words()
-        print("Filtered words count: " + str(len(filtered_words)))
-        print(random.choices(filtered_words, k=min(5, len(filtered_words))))
+        guess, total_potentials = filter_words()
+
+        if total_potentials == 1:
+            print("We win! The word is: " + str(guess))
+            sys.exit()
+
+        print("Filtered words count: " + str(total_potentials))
+        print(guess)
+        max_guesses -= 1
+
+    print("Boo! :(")
+    sys.exit()
